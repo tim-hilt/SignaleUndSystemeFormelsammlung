36c8c0ddce6d51c0cdf9cc5e5a7ae8bf70a3b823
Tim Hilt <timhilt@live.de>
2019-01-11 11:12:26 +0100

    Initial Commit

---
+++ auto/FormelsammlungSuS.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "FormelsammlungSuS"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "--shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "12pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl12"
+    "mystyle"
+    "siunitx"))
+ :latex)
+
