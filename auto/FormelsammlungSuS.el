d2da317abd7d70f9ec93a352868f0d7d8118d314
Tim Hilt <timhilt@live.de>
2019-01-16 21:19:58 +0100

    Changed structure, added a lot of stuff

--- auto/FormelsammlungSuS.el
+++ auto/FormelsammlungSuS.el
@@ -4,13 +4,16 @@
    (setq TeX-command-extra-options
          "--shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("scrartcl" "12pt" "a4paper")))
+                     '(("scrartcl" "12pt" "a4paper" "twoside")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("circuitikz" "europeanresistors" "americaninductors")))
    (TeX-run-style-hooks
     "latex2e"
     "scrartcl"
     "scrartcl12"
     "siunitx"
     "circuitikz"
-    "mystyle"))
+    "mystyle"
+    "scrlayer-scrpage"))
  :latex)
 
